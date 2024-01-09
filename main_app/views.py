b7d6977a7d3edd88a5ab9802a5a00bf969d9fb87
honeybee <shelby.e.pagan@gmail.com>
2024-01-09 15:53:49 -0500

    part 1 complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+finches = [
+  {'name': 'Hoot', 'family': 'Owl Finch', 'description': 'curious and gregarious', 'age': 3},
+  {'name': 'Casa', 'family': 'House Finch', 'description': 'social butterfly', 'age': 2},
+  {'name': 'Queenie', 'family': 'Diamond Firetail', 'description': 'sassy and a touch aggressive', 'age': 2},
+]
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
